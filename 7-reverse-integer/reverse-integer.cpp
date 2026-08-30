1f738ece3eed0b5e9a14dc9b5bf58906a992201a
Yash Deshpande <87467911+YashDeshpande006@users.noreply.github.com>
2026-08-30 08:40:48 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (20.47%) - LeetSync

---
+++ 7-reverse-integer/reverse-integer.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int reverse(int x) {
+        int copy = x;
+        int reverse = 0;
+        int digit = 0;
+        while (x != 0) {
+            digit = x % 10;
+            if ((reverse > INT_MAX / 10) || (reverse < INT_MIN / 10)) {
+                return 0;
+            }
+            reverse = reverse * 10 + digit;
+            x = x / 10;
+        }
+        return reverse;
+    }
+};
