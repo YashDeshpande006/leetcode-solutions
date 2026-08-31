a0297843ce6ce617be9303610981d80e7b6d7486
Yash Deshpande <87467911+YashDeshpande006@users.noreply.github.com>
2026-08-31 23:01:07 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (92.36%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size() != t.size()){
+            return false;
+        }
+        int count[26]={0};
+
+        for(int i=0;i<s.size();i++){
+            count[s[i]-'a']++;
+            count[t[i]-'a']--;
+        }
+        for(int j=0;j<26;j++){
+            if(count[j]!=0){
+                return false;
+            }
+        }
+        return true;
+    }
+};
