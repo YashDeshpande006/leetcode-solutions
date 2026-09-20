0b2719e4fb4d9cf6bbcfc3f9b926ddd7d09d0df5
Yash Deshpande <87467911+YashDeshpande006@users.noreply.github.com>
2026-09-20 20:11:45 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (31.21%) - LeetSync

---
+++ 2491-smallest-even-multiple/smallest-even-multiple.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int smallestEvenMultiple(int n) {
+        int ans=0;
+        if(n%2!=0){
+            ans=n*2;
+        }
+        else{
+            ans=n;
+        }
+        return ans;
+    }
+};
